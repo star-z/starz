91c2259c92c74d62eaba411519a9d793fb490dc2
jameszmartin <jameszwilliammartin@gmail.com>
2015-04-02 11:34:33 +1100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
-var gulp       = require('gulp'),
-    browserify = require('gulp-browserify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+ 
+gulp.task('scripts', function() {
+    return browserify('app/client/grid-client.js','app/client/todos-client.js','app/client/products-client.js')
+        .bundle()
+        //Pass desired output filename to vinyl-source-stream  .pipe(source('bundle.js'))
+        // Start piping stream to tasks!
+        .pipe(gulp.dest('./public/scripts/'));
+});
 
+/*
 gulp.task('scripts', function () {
 
     gulp.src(['app/client/grid-client.js','app/client/todos-client.js','app/client/products-client.js' ])
@@ -11,6 +21,6 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('./public/scripts/'));
 
 });
+*/
 
 gulp.task('default', ['scripts']);
-

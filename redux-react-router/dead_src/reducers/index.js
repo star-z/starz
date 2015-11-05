594dd8fffabab294eb944f371d345c1ba566b7d9
Steven Iseki Martin <stevenisekimartin@gmail.com>
2015-11-05 16:23:53 +1100

    update to react router example

--- redux-react-router/dead_src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore, compose, combineReducers } from 'redux';
-import { routerStateReducer } from 'redux-router';
-
-export const reducer = combineReducers({
-  router: routerStateReducer
-});

8324d7c6ab467c2c055e575c4846f642a687b6a6
StevenIseki <stevenisekimartin@gmail.com>
2016-05-18 11:31:25 +1000

    update to routes

--- api/routes/series.js
+++ api/routes/series.js
@@ -7,7 +7,7 @@ function get(req, res){
           id: 1,
           title: 'American Dad!',
           published_at: '2016-04-19T10:00:18.637Z',
-          screenshot: 'http://www.ew.com/sites/default/files/styles/tout_image_612x380/public/i/2014/11/18/American-Dad_612x380.jpg?itok=wOjKOMNk',
+          screenshot: 'http://imgur.com/Ka0Ezcg.jpg',
           description: 'American Dad! from Family Guy creator, Seth MacFarlane, is the animated story of Stan Smith, who works for the CIA and is constantly on the alert for terrorist activity',
           category: 'comedy',
           user_name: 'Stan Smith'
@@ -16,7 +16,7 @@ function get(req, res){
           id: 2,
           title: 'The Simpsons',
           published_at: '2016-04-19T10:00:18.637Z',
-          screenshot: 'http://www.wired.com/wp-content/uploads/2015/05/the-simpsons.jpg',
+          screenshot: 'http://imgur.com/Ka0Ezcg.jpg',
           description: 'The Simpsons is an American animated sitcom created by Matt Groening for the Fox Broadcasting Company',
           category: 'comedy',
           user_name: 'Homer Simpson'
